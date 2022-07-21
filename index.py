83c04b8f77fbe685a191d22532ab71e55af24f6a
HamishDrogemuller <79230862+HamishDrogemuller@users.noreply.github.com>
2022-07-22 11:41:56 +1200

    Calc Function

--- index.py
+++ index.py
@@ -6,10 +6,10 @@ print(hello)
 def add_num(a,b):#function for addition
     sum=a+b;
     return sum; #return value
-num1=int(input("input number one: "))#input from user for num1
-num2=int(input("input number two:"))#input from user for num2
+#num1=int(input("input number one: "))#input from user for num1
+#num2=int(input("input number two:"))#input from user for num2
 
-print("The sum is",add_num(num1,num2))#call the function
+#print("The sum is",add_num(num1,num2))#call the function
 
 #Subtraction Function
 
@@ -17,10 +17,10 @@ def sub_num(a,b):#function for subtraction
     sub=a-b;
     return sub; #return value
 
-num1=int(input("input number one: "))#input from user for num1
-num2=int(input("input number two:"))#input from user for num2
+#num1=int(input("input number one: "))#input from user for num1
+#num2=int(input("input number two:"))#input from user for num2
 
-print("The subtraction is",sub_num(num1,num2))#call the function
+#print("The subtraction is",sub_num(num1,num2))#call the function
 
 #Multiplication Function
 
@@ -28,10 +28,10 @@ def mul_num(a,b):#function for multiplication
     mul=a*b;
     return mul; #return value
 
-num1=int(input("input number one: "))#input from user for num1
-num2=int(input("input number two:"))#input from user for num2
+#num1=int(input("input number one: "))#input from user for num1
+#num2=int(input("input number two:"))#input from user for num2
 
-print("The multiplication is",mul_num(num1,num2))#call the function
+#print("The multiplication is",mul_num(num1,num2))#call the function
 
 #Division Function
 
@@ -39,9 +39,27 @@ def div_num(a,b):#function for division
     div=a/b;
     return div; #return value
 
-num1=int(input("input number one: "))#input from user for num1
-num2=int(input("input number two:"))#input from user for num2
+#num1=int(input("input number one: "))#input from user for num1
+#num2=int(input("input number two:"))#input from user for num2
 
-print("The division is",div_num(num1,num2))#call the function
+#print("The division is",div_num(num1,num2))#call the function
 
+#Calculator
+
+print("Select Operation: +, -, *, /")
+Operator = Input("Enter the operator: ")
+
+if choice in ('+', '-', '*', '/'):
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    if Operator == '+':
+        print(num1, "+", num2, "=", add_num(num1, num2))
+    elif Operator == '-':
+        print(num1, "-", num2, "=", sub_num(num1, num2))
+    elif Operator == '*':
+        print(num1, "*", num2, "=", mul_num(num1, num2))
+    elif Operator == '/':
+        print(num1, "/", num2, "=", div_num(num1, num2))
+    else:
+        print("Invalid operator")
 
