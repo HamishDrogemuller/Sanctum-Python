564a2a15affeaa0a4172c6d53d06d93687d55118
HamishDrogemuller <79230862+HamishDrogemuller@users.noreply.github.com>
2022-07-24 15:33:36 +1200

    Finances Start

---
+++ Finances.py
@@ -0,0 +1,10 @@
+'''
+Code to calculate finances
+'''
+
+Income = input("Enter your income: ")
+Rent = input("Enter your rent: ")
+Utilities = input("Enter your utilities: ")
+Food = input("Enter your food: ")
+Transport = input("Enter your transport: ")
+
